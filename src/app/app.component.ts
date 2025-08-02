6fac6c790aac80f0d433c8fdbf87e192c79c6c4b
asirko <sirko.alexandre@gmail.com>
2025-08-02 02:08:13 +0200

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { ImageLoader } from './components/image-loader/image-loader.component';
 import { ColorsPickerComponent } from './components/colors-picker/colors-picker.component';
 import { LayeredImageComponent } from './components/layered-image/layered-image.component';
-import { RGB } from './interfaces/colors.interface';
-import { getColorDistance, transformRGBToHex } from './utils/colors.utils';
-import { extractMainColorsByKMeans } from './logic/extract-main-colors.logic';
 import { PrintParamsComponent } from './components/print-params/print-params.component';
 
 @Component({
@@ -18,43 +15,16 @@ export class AppComponent {
   readonly imageData = signal<ImageData | undefined>(undefined);
   readonly colors = signal<string[]>(['#ffffff', '#000000']);
 
-  guesstimateColors() {
-    let pixels: RGB[] = [];
-    const imageDataArray = this.imageData()!.data;
-    for (let i = 0; i < imageDataArray.length; i += 4) {
-      pixels.push([imageDataArray[i], imageDataArray[i + 1], imageDataArray[i + 2]]);
-    }
-    const colorsSelected: RGB[] = [];
-    for (let i = 0; i < 8; i++) {
-      const mostPresentColor = this.#getMostPresentColor(pixels);
-      colorsSelected.push(mostPresentColor);
-      pixels = pixels.filter(pixel => getColorDistance(pixel, mostPresentColor) > 20);
-    }
-    this.colors.set(colorsSelected.map(transformRGBToHex));
-  }
+  readonly steps = [
+    { icon: 'image', label: "Choix de l'image" },
+    { icon: 'tune', label: 'Paramétrage' },
+    { icon: 'palette', label: 'Couleurs' },
+    { icon: 'visibility', label: 'Aperçu' },
+    { icon: 'view_in_ar', label: 'Export 3D' },
+  ];
+  readonly activeStep = signal(0);
 
-  #getMostPresentColor(pixels: RGB[]): RGB {
-    const colorsCount = new Map<string /*color hex3*/, number /*nb pixels with such color*/>();
-    for (let pixel of pixels) {
-      const color16 = pixel.map(c => Math.floor(c / 16));
-      const hex3 = `#${color16.map(c => c.toString(16)).join('')}`;
-      if (!colorsCount.has(hex3)) {
-        colorsCount.set(hex3, 1);
-      } else {
-        colorsCount.set(hex3, colorsCount.get(hex3)! + 1);
-      }
-    }
-    const mostPresentHex3 = [...colorsCount.entries()]
-      .map(([hex3, nbPixel]) => ({ hex3, nbPixel }))
-      .reduce((acc, curr) => (acc.nbPixel < curr.nbPixel ? curr : acc));
-
-    return mostPresentHex3.hex3
-      .slice(1)
-      .split('')
-      .map(c => parseInt(c + c, 16)) as RGB;
-  }
-
-  kMeans() {
-    this.colors.set(extractMainColorsByKMeans(this.imageData()!, 5));
+  setStep(index: number) {
+    this.activeStep.set(index);
   }
 }
--- src/app/components/colors-picker/colors-picker.component.ts
+++ src/app/components/colors-picker/colors-picker.component.ts
@@ -2,6 +2,9 @@ import { ChangeDetectionStrategy, Component, inject, input, OnChanges, output } 
 import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { moveItemInArray } from '@angular/cdk/drag-drop';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { RGB } from '../../interfaces/colors.interface';
+import { getColorDistance, transformRGBToHex } from '../../utils/colors.utils';
+import { extractMainColorsByKMeans } from '../../logic/extract-main-colors.logic';
 
 @Component({
   selector: 'lc-colors-picker',
@@ -10,11 +13,8 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
   styleUrl: './colors-picker.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ColorsPickerComponent implements OnChanges {
+export class ColorsPickerComponent {
   readonly #fb = inject(FormBuilder);
-
-  readonly colorsChange = output<string[]>();
-  readonly colors = input<string[]>();
 
   readonly colorArray = this.#fb.nonNullable.array<FormControl<string>>([]);
 
@@ -37,5 +37,45 @@ export class ColorsPickerComponent implements OnChanges {
 
   moveColor(fromIndex: number, toIndex: number) {
     moveItemInArray(this.colorArray.controls, fromIndex, toIndex);
+  }
+
+  guesstimateColors() {
+    let pixels: RGB[] = [];
+    const imageDataArray = this.imageData()!.data;
+    for (let i = 0; i < imageDataArray.length; i += 4) {
+      pixels.push([imageDataArray[i], imageDataArray[i + 1], imageDataArray[i + 2]]);
+    }
+    const colorsSelected: RGB[] = [];
+    for (let i = 0; i < 8; i++) {
+      const mostPresentColor = this.#getMostPresentColor(pixels);
+      colorsSelected.push(mostPresentColor);
+      pixels = pixels.filter(pixel => getColorDistance(pixel, mostPresentColor) > 20);
+    }
+    this.colors.set(colorsSelected.map(transformRGBToHex));
+  }
+
+  #getMostPresentColor(pixels: RGB[]): RGB {
+    const colorsCount = new Map<string /*color hex3*/, number /*nb pixels with such color*/>();
+    for (let pixel of pixels) {
+      const color16 = pixel.map(c => Math.floor(c / 16));
+      const hex3 = `#${color16.map(c => c.toString(16)).join('')}`;
+      if (!colorsCount.has(hex3)) {
+        colorsCount.set(hex3, 1);
+      } else {
+        colorsCount.set(hex3, colorsCount.get(hex3)! + 1);
+      }
+    }
+    const mostPresentHex3 = [...colorsCount.entries()]
+      .map(([hex3, nbPixel]) => ({ hex3, nbPixel }))
+      .reduce((acc, curr) => (acc.nbPixel < curr.nbPixel ? curr : acc));
+
+    return mostPresentHex3.hex3
+      .slice(1)
+      .split('')
+      .map(c => parseInt(c + c, 16)) as RGB;
+  }
+
+  kMeans() {
+    this.colors.set(extractMainColorsByKMeans(this.imageData()!, 5));
   }
 }
